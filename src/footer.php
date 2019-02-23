416e69faa3928906d04e76c82b1c4e3f341a3750
kkgigiioo <47388662+kkgigiioo@users.noreply.github.com>
2019-02-23 11:28:18 +0100

    Merge branch 'master' of https://github.com/vuk-120/V_Org

--- src/footer.php
+++ src/footer.php
@@ -1,3 +1,3 @@
-<footer align="center" style="position: fixed; bottom: 0; border-top: solid 1px; width: 100%;">
-V_ &copy; 2019
+<footer align="center" style="position: absolute; bottom: 0; border-top: solid 1px; left: -2%; right: -2%;">
+V_ © 2019
 </footer>
