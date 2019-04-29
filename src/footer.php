cbe0dd4f181559a2ebe5760c8546973f4582dd03
Csanaki Richárd <richard.csanaki@gmail.com>
2019-04-29 14:46:18 +0200

    delete all the shit

--- src/footer.php
+++
@@ -1,3 +0,0 @@
-<footer align="center" style="position: fixed; bottom: 0; border-top: solid 1px; width: 100%;">
-V_ &copy; 2019
-</footer>
--- src/logOffProcess.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-	
-	$_SESSION['name']="";
-	$_SESSION['success']=false;
-	
-	header("Location:index.php");
-	
-?>
